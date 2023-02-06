d1b96340a8813df1b5c8c621854e9612e01a1fa3
Vadim <79169082210@mail.ru>
2023-02-06 20:51:21 +0300

    добавил Svg loader. File loader. Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,21 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -33,5 +48,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         typescriptLoader,
         cssLoader,
+        svgLoader,
+        fileLoader,
     ]
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { classNames } from "../../../shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 
@@ -10,6 +11,7 @@ export const Navbar = ({className}: NevbarProps) => {
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink 
                     theme={AppLinkTheme.SECONDARY} 
