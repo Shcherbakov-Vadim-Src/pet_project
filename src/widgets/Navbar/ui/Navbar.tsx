f162d7b7c25649f2de052df46c06b7277655044d
Vadim <79169082210@mail.ru>
2023-02-06 10:27:56 +0300

    добавил первый виджет Navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,31 @@
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { classNames } from "../../../shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+
+interface NevbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NevbarProps) => {
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink 
+                    theme={AppLinkTheme.SECONDARY} 
+                    to={'/'} 
+                    className={cls.mainLink}
+                >
+                    Главная
+                </AppLink>
+                <AppLink 
+                    theme={AppLinkTheme.SECONDARY} 
+                    to={'/about'}
+                >
+                    О сайте
+                </AppLink>
+            </div>
+            
+        </div>
+    );
+};
